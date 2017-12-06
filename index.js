0a966192685da9d5075532606ecf82c381e63cb8
Rob Dowers <rdowers@gmail.com>
2017-12-06 23:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ function removeFromCart(item) {
   
   for (i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      return cart.splice(i,1);
+      cart.splice(i,1);
+      return cart;
     }
   }
    return "That item is not in your cart.";
