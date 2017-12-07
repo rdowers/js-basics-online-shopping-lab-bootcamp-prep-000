aa3287363cee63a205685704437592eb13a57133
Rob Dowers <rdowers@gmail.com>
2017-12-07 00:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,8 +84,8 @@ console.log("That item is not in your cart.");
 function placeOrder(cardNumber) {
 
 if (Boolean(cardNumber)) {
-console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
-return;
+var response = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+return response;
   }
   console.log("Sorry, we don't have a credit card on file for you.");
 }
