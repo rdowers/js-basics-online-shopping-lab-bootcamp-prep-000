4953c3e1c251a12b1cb7317b05c86b93fbcd4bbf
Rob Dowers <rdowers@gmail.com>
2017-12-07 00:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function placeOrder(cardNumber) {
 
 if (Boolean(cardNumber)) {
 var response = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
-console.log(response);
+return response;
   }
   console.log("Sorry, we don't have a credit card on file for you.");
 }
